21bd1934619a3f8258b9e5723429ce026335f911
Jussi Patana <jussi@patana.info>
2021-04-18 20:48:00 +0300

    finishing sign in handlesubmit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -7,57 +7,65 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 
 class App extends React.Component {
-constructor() {
-  super();
-  this.state ={
-    currentUser: null
-  }
-}
+    constructor() {
+        super();
+        this.state = {
+            currentUser: null
+        }
+    }
 
-unsubscribeFromAuth = null
+    unsubscribeFromAuth = null
 
-componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id:snapShot.id,
-            ...snapShot.data()
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+                });
             }
-          });
-          console.log(this.state);
+            this.setState({ currentUser: userAuth });
+
         });
-      }
-      this.setState({currentUser: userAuth});
-      
-  });
-}
+    }
 
-componentWillUnmount() {
-  this.unsubscribeFromAuth();
-}
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
 
-render() {
-  return (
-    <div>
-    <Header currentUser={this.state.currentUser}/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
-}
+    render() {
+        return ( <
+            div >
+            <
+            Header currentUser = { this.state.currentUser }
+            /> <
+            Switch >
+            <
+            Route exact path = '/'
+            component = { HomePage }
+            /> <
+            Route exact path = '/shop'
+            component = { ShopPage }
+            /> <
+            Route exact path = '/signin'
+            component = { SignInAndSignUpPage }
+            /> <
+            /Switch> <
+            /div>
+        );
+    }
 
-  
+
 }
 
 export default App;
