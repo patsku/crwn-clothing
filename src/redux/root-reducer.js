5b8c6cd0e9b91227fd333ada5a9edd01716128ea
Jussi Patana <jussi@patana.info>
2021-04-20 21:27:27 +0300

    cart-icon and cart-dropdown components added also cart reducer implemented

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import userReducer from './user/user.redurer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
